9c8a3f8aefd9fd3a5aab44e0736595771715d5bf
Bruno Aggierni <bruno.aggierni@gmail.com>
2021-09-18 19:50:28 +0100

    Conditional rendering done

--- src/Input.svelte
+++ src/Input.svelte
@@ -1,20 +1,43 @@
 <script>
   let value = "";
   let totalMovies = []
+  let loading = true
   const handleInput = (event) => {
     value = event.target.value;
   };
 
   $: if (value.length > 2) {
+    loading = true
     async function fetchMovies() {
       // waits until the request completes...
       const response = await fetch(`http://www.omdbapi.com/?s=${value}&apikey=3b970e59`)
       const apiResponse = await response.json()
-      totalMovies = apiResponse.Search
+      totalMovies = apiResponse.Search || []
+      loading = false
     }
     fetchMovies ()
-      }
-    
+  }
+  
 </script>
 
-<input {value} on:input={handleInput} />
+<main>
+<input 
+ placeholder="Search for a movie"
+ value = {value} 
+ on:input={handleInput} />
+
+{#if loading}
+<strong>Loading ...</strong>
+{:else}
+{#if totalMovies.length > 0}
+<strong>We found {totalMovies.length} movies</strong>
+{:else}
+<strong>We found no movies</strong>
+{/if}
+{/if}
+  <!-- {  
+  totalMovies.length > 0 ? `We found ${totalMovies.length} movies` : 'We found no movies'
+      
+  } -->
+
+</main>
