fe82d1c075d946dbdd4d59ea93bf6aed4175a9e3
Bruno Aggierni <bruno.aggierni@gmail.com>
2021-09-19 19:53:29 +0100

    res.ok sends error, fix that things bro !

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,26 @@
 <script>
-	import Counter from './Counter.svelte'
-	import Input from "./Input.svelte";
+  import Counter from "./Counter.svelte";
+  import Input from "./Input.svelte";
 
-	const commonProps = {maxCounter : 5}
+  const commonProps = { maxCounter: 5 };
 </script>
 
 <main>
-<Input/>
-<Counter count = {4}></Counter>
-<Counter maxCounter = {8}></Counter>	
-<Counter maxCounter = {6}></Counter>
+	<h1>Search Movies 🍿</h1>
+  <Input />
+  <Counter count={4} />
+  <Counter maxCounter={8} />
+  <Counter maxCounter={6} />
 </main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	h1 {
+		font-size: 32px;
+	}
+</style>
--- src/Input.svelte
+++ src/Input.svelte
@@ -1,48 +1,35 @@
 <script>
-  import Movie from './Movie.svelte'
+  import Movie from "./Movie.svelte";
   let value = "";
-  let totalMovies = []
-  let loading = true
+  let response = [];
+
   const handleInput = (event) => {
     value = event.target.value;
   };
 
   $: if (value.length > 2) {
-    loading = true
-    async function fetchMovies() {
-      // waits until the request completes...
-      const response = await fetch(`http://www.omdbapi.com/?s=${value}&apikey=3b970e59`)
-      const apiResponse = await response.json()
-      totalMovies = apiResponse.Search || []
-      loading = false
-    }
-    fetchMovies ()
+    response = fetch(`http://www.omdbapi.com/?s=${value}&apikey=3b970e59`)
+    .then((res) => !res.ok() && new Error("theref asfa fas hr hrt "))
+      .then((res) => res.json())
+      .then((apiResponse) => apiResponse.Search || []);
   }
-  
 </script>
 
-<main>
-<input 
- placeholder="Search for a movie"
- value = {value} 
- on:input={handleInput} />
+<input placeholder="Search for a movie" value = {value} on:input={handleInput} />
 
-{#if loading}
-<strong>Loading ...</strong>
-{:else}
-  {#each totalMovies as {Title, Poster, Year} , index}
-    <Movie
-    index = {index}
-    title = {Title}
-    poster = {Poster}
-    year = {Year}/>
-{:else}
-<strong>We found no movies</strong>
-{/each}
-{/if}
-  <!-- {  
-  totalMovies.length > 0 ? `We found ${totalMovies.length} movies` : 'We found no movies'
-      
-  } -->
-
-</main>
+{#await response}
+  <strong>Loading ...</strong>
+{:then movies}
+  {#each movies as { Title, Poster, Year }, index}
+    <Movie 
+    index={index} 
+    title={Title} 
+    poster={Poster} 
+    year={Year} 
+    />
+  {:else}
+    <strong>We found no movies</strong>
+  {/each}
+{:catch error}
+  <p>❌ There has been an {error}</p>
+{/await}
